742513bfee6ad9b2bddc27cc91fac3e721e94f73
danilkolikov <danilkolikov@gmail.com>
2015-11-19 20:01:56 +0300

    little

--- app/src/main/java/util/database/DatabaseActions.java
+++ app/src/main/java/util/database/DatabaseActions.java
@@ -3,8 +3,6 @@ package util.database;
 import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.provider.BaseColumns;
-import android.provider.SyncStateContract;
 
 import java.util.ArrayList;
 
