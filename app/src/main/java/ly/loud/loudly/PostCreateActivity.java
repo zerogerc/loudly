c6e57324f491d8d11d8ceb416a10c6386934736d
Vladislav Sazanovich <zerogerc@gmail.com>
2015-11-19 19:01:13 +0300

    GUI update

--- app/src/main/java/ly/loud/loudly/PostCreateActivity.java
+++ app/src/main/java/ly/loud/loudly/PostCreateActivity.java
@@ -1,27 +1,26 @@
 package ly.loud.loudly;
 
-import android.content.Context;
 import android.os.Bundle;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class PostCreateActivity extends AppCompatActivity {
     private EditText editText;
-    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_post);
-        context = this;
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.post_toolbar); // Attaching the layout to the main_toolbar object
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         editText = (EditText)findViewById(R.id.post_edit_text);
 
@@ -31,18 +30,15 @@ public class PostCreateActivity extends AppCompatActivity {
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                if (editText.getCurrentTextColor() == ContextCompat.getColor(context, R.color.light_grey_color)) {
-                    if (!s.toString().equals("What's on your mind?")) {
-                        editText.setTextColor(ContextCompat.getColor(context, R.color.black_color));
-                        CharSequence text = s.subSequence(start, start + count);
-                        editText.setText("");
-                        editText.append(text);
-                    }
-                }
             }
 
             @Override
             public void afterTextChanged(Editable s) {}
         });
+    }
+
+    public void clickToast(View v) {
+        Toast toast = Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT);
+        toast.show();
     }
 }
