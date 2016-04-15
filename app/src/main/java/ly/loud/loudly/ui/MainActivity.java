a2f792f5bf171b54222dbf58cdc151940c4f1a3e
Vladislav Sazanovich <zerogerc@gmail.com>
2016-04-15 21:54:32 +0300

    drawer_layout change

--- app/src/main/java/ly/loud/loudly/ui/MainActivity.java
+++ app/src/main/java/ly/loud/loudly/ui/MainActivity.java
@@ -196,7 +196,7 @@ public class MainActivity extends AppCompatActivity
 
         }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.fab);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
